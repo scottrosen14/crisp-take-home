d3ba1377cb3df1a0a9d74345879fcb585b7c55a5
Scott Rosen <scott.rosen14@gmail.com>
2025-04-13 22:52:47 -0500

    Create GroupMetricRows from GroupRows

---
+++ src/components/GroupRows/GroupMetricRows/GroupMetricRows.tsx
@@ -0,0 +1,74 @@
+import React, { ReactElement } from 'react';
+import {
+  tdStyle,
+  categoryStyle as groupStyle,
+} from '../../PivotBody/PivotBody.styles';
+import { ColumnMetrics } from '../../../constants/constants';
+
+type SubGroupEntry = [string, ColumnMetrics];
+
+interface Props {
+  groupName: string;
+  subGroups: Record<string, ColumnMetrics>;
+  columns: string[];
+}
+
+const GroupMetricRows = ({
+  groupName,
+  subGroups,
+  columns,
+}: Props): ReactElement => {
+  return (
+    <>
+      {Object.entries(subGroups).map(
+        ([subGroupName, columnMetrics]: SubGroupEntry, index) => {
+          const rowTotal = columns.reduce(
+            (sum: number, columnName: string) =>
+              sum + (columnMetrics[columnName] || 0),
+            0
+          );
+
+          return (
+            <tr
+              key={`${groupName}-${subGroupName}`}
+              data-testid={`${groupName}-${subGroupName}-group-metric-rows`}
+            >
+              {index === 0 && (
+                <td
+                  data-testid={`${groupName}-group-name`}
+                  style={{ ...tdStyle, ...groupStyle }}
+                  rowSpan={Object.keys(subGroups).length + 1}
+                >
+                  {groupName}
+                </td>
+              )}
+              <td
+                data-testid={`${groupName}-${subGroupName}-subgroup-name`}
+                style={tdStyle}
+              >
+                {subGroupName}
+              </td>
+              {columns.map(columnName => (
+                <td
+                  data-testid={`${groupName}-${subGroupName}-${columnName}-metric`}
+                  key={columnName}
+                  style={tdStyle}
+                >
+                  {columnMetrics[columnName]?.toLocaleString() || '-'}
+                </td>
+              ))}
+              <td
+                data-testid={`${groupName}-${subGroupName}-row-total`}
+                style={{ ...tdStyle, fontWeight: 500 }}
+              >
+                {rowTotal.toLocaleString()}
+              </td>
+            </tr>
+          );
+        }
+      )}
+    </>
+  );
+};
+
+export default GroupMetricRows;
--- src/components/GroupRows/GroupRows.tsx
+++ src/components/GroupRows/GroupRows.tsx
@@ -5,11 +5,10 @@ import {
   totalRowStyle,
 } from '../PivotBody/PivotBody.styles';
 import GroupTotalRow from './GroupTotalRow/GroupTotalRow';
+import GroupMetricRows from './GroupMetricRows/GroupMetricRows';
 import { useAppSelector } from '../../redux/store';
 import { selectGroupTotals } from '../../redux/features/orders/ordersSelectors';
 import { SubGroups, ColumnMetrics } from '../../constants/constants';
-
-type SubGroupEntry = [string, ColumnMetrics];
 
 interface Props {
   groupName: string;
@@ -24,53 +23,11 @@ const GroupRows = ({ groupName, subGroups, columns }: Props): ReactElement => {
 
   return (
     <Fragment data-testid={`${groupName}-group-rows`} key={groupName}>
-      {Object.entries(subGroups).map(
-        ([subGroupName, columnMetrics]: SubGroupEntry, index) => {
-          const rowTotal = columns.reduce(
-            (sum: number, columnName: string) =>
-              sum + (columnMetrics[columnName] || 0),
-            0
-          );
-
-          return (
-            <tr
-              key={`${groupName}-${subGroupName}`}
-              data-testid={`${groupName}-${subGroupName}-grouped-row`}
-            >
-              {index === 0 && (
-                <td
-                  data-testid={`${groupName}-${subGroupName}-group-name`}
-                  style={{ ...tdStyle, ...groupStyle }}
-                  rowSpan={Object.keys(subGroups).length + 1}
-                >
-                  {groupName}
-                </td>
-              )}
-              <td
-                data-testid={`${groupName}-${subGroupName}-subgroup-name`}
-                style={tdStyle}
-              >
-                {subGroupName}
-              </td>
-              {columns.map(columnName => (
-                <td
-                  data-testid={`${groupName}-${subGroupName}-${columnName}-metric`}
-                  key={columnName}
-                  style={tdStyle}
-                >
-                  {columnMetrics[columnName]?.toLocaleString() || '-'}
-                </td>
-              ))}
-              <td
-                data-testid={`${groupName}-${subGroupName}-row-total`}
-                style={{ ...tdStyle, fontWeight: 500 }}
-              >
-                {rowTotal.toLocaleString()}
-              </td>
-            </tr>
-          );
-        }
-      )}
+      <GroupMetricRows
+        groupName={groupName}
+        subGroups={subGroups}
+        columns={columns}
+      />
 
       <GroupTotalRow
         groupTotalsByColumn={groupTotalsByColumn}
